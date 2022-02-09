422c7b1d9d334742814742d03136108f24e99eef
osvaldo arruda <osvaldo.ark@gmail.com>
2022-02-08 22:37:16 -0300

    mudando para o git do jackson

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'jacksonangelo',
+  linkedin: 'in/jackson-angelo',
+  facebook: '100008837327888',
+  instagram: 'jackangelo_',
+  twitter: 'jacksonangelo24'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
